7bdb8240326d82be9bc6f5a6c1ebb9e579e9ea80
Klemjl <juliaklemenchuck06@gmail.com>
2026-02-10 23:25:35 +0200

    Added wrapper in app.main to expose battle

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 from app.knights.knight_info import KNIGHTS
 from app.fights.battle_prep import prepare_knights
-from app.fights.battle import battle
+from app.fights.battle import battle as _impl_battle
+
+
+def battle(*args, **kwargs) -> dict:
+    return _impl_battle(*args, **kwargs)
 
 
 def main() -> None:
     knights = prepare_knights(KNIGHTS)
     results = battle(knights)
-    print(results)
+
+    if __name__ == "__main__":
+        print(results)
 
 
 if __name__ == "__main__":
